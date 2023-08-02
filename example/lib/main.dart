ed814a29000f3d37a1f72ac2e719a99754ba840c
ashburger <ashwin@visualogic.com.my>
2023-08-02 16:39:02 +0800

    update name

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 
-import 'package:form_builder_image_picker/form_builder_image_picker.dart';
+import 'package:custom_form_builder_image_picker/form_builder_image_picker.dart';
 
 void main() {
   runApp(const MyApp());
